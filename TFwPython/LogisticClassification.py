b42dabe91c832301001e1ac8f021b78befa6b1c5
yeoshim <yeoshim@gmail.com>
2016-04-24 15:04:26 +0900

    add train2.txt

--- TFwPython/LogisticClassification.py
+++ TFwPython/LogisticClassification.py
@@ -1,26 +1,22 @@
 import tensorflow as tf
 import numpy as np
 
-xy = np.loadtxt('train.txt', unpack=True, dtype='float32')
+xy = np.loadtxt('train2.txt', unpack=True, dtype='float32')
 x_data = xy[0:-1]
 y_data = xy[-1];
 
-# x_data = [[1, 1, 1, 1, 1],
-#           [1., 0., 3., 0., 5.],
-#           [0., 2., 0., 4., 0.]]
-# y_data = [1., 2., 3., 4., 5.]
-
-# W = tf.Variable( tf.random_uniform([1,len(x_data)], -5.0, 5.0) )
-W = tf.Variable( tf.random_uniform([1,len(x_data)], -1.0, 1.0) )
-
 X = tf.placeholder(tf.float32)
 Y = tf.placeholder(tf.float32)
+W = tf.Variable( tf.random_uniform([1,len(x_data)], -1.0, 1.0) )
 
-hypothesis = tf.matmul(W, X)
-# hypothesis = tf.matmul(W, X) + b
-# hypothesis = W1 * X1 + W2 * X2 + b
+# Hypothesis based on sigmoid
+# H(X) = 1 / 1 + e^(-WX)
+z = tf.matmul(W, X)
+hypothesis = tf.div( 1., 1. + tf.exp(-z) )
 
-cost = tf.reduce_mean( tf.square(hypothesis - Y) )
+# Cost func based on Cross-entropy
+# -1/m sigma( ylog(H(x)) + (1-y)log(1-H(x) )
+cost = -tf.reduce_mean( Y * tf.log(hypothesis) + (1-Y)*tf.log(1-hypothesis) )
 
 a = tf.Variable( 0.1 )
 optimizer = tf.train.GradientDescentOptimizer(a)
@@ -35,5 +31,11 @@ for step in xrange(2001):
     sess.run( train, feed_dict={X:x_data, Y:y_data} )
     if step % 20 == 0:
         print step, sess.run(cost, feed_dict={X:x_data, Y:y_data}), sess.run(W)
-# Learns best fit is W:[1,1], b:[0]
+
+print '-------------------------------------------'
+# study_hour attendance
+print sess.run( hypothesis, feed_dict={X:[[1], [2], [2]]} ) > 0.5
+print sess.run( hypothesis, feed_dict={X:[[1], [5], [5]]} ) > 0.5
+
+print sess.run( hypothesis, feed_dict={X:[[1,1], [4,3], [3,5]]} ) > 0.5
         
